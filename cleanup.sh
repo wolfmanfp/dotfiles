ec044d4933c925fff27fb8605dbac1d762f3aec1
Farkas Péter <peter@wolfman.hu>
2025-08-21 15:23:29 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # APT cache ürítése
-apt clean
+sudo apt clean
 
 # Cache ürítése
 find ~/.cache/ -type f -atime +1 -delete
@@ -13,9 +13,9 @@ find ~/.m2/repository/ -atime +120 -iname '*.pom' | while read pom; do parent=`d
 set -eu
 snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
-        snap remove "$snapname" --revision="$revision"
+        sudo snap remove "$snapname" --revision="$revision"
     done
     
 # Flatpak
-flatpak uninstall --unused | yes
+sudo flatpak uninstall --unused | yes
 sudo rm -rfv /var/tmp/flatpak-cache-*
